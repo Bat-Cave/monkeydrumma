4e916e666af5268a23011bcc142c84a8ea1b7567
Richard Hancock <richardl@hancock1.com>
2021-10-05 23:53:18 -0600

    changed components folder name

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./components/Home";
-import Shop from "./components/Shop";
-import ViewCollection from "./components/ViewCollection";
-import BossSkins from "./components/BossSkins";
-import TwitchBotInfo from "./components/TwitchBotInfo";
-import SoundAlerts from "./components/SoundAlerts";
+import Home from "./Components/Home";
+import Shop from "./Components/Shop";
+import ViewCollection from "./Components/ViewCollection";
+import BossSkins from "./Components/BossSkins";
+import TwitchBotInfo from "./Components/TwitchBotInfo";
+import SoundAlerts from "./Components/SoundAlerts";
 
 export default (
   <Switch>
